a3c43ada771e25ca0f02e27bb4c6789560cf4ba4
Dongmyeong LEE <ldmldm1119@gmail.com>
2023-12-04 22:52:05 +0900

    #14 - DB test 불필요 코드 삭제
    강의 내용 이외에 개인적으로 테스트 했던 내용이 포함되어 있어 삭제함

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 
@@ -16,7 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
-@Rollback(value = false)
 class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
